f2bb0c4ee07a9b082b23969a54844977d76ddd6f
andreandres <andresoaressdev@gmail.com>
2026-05-31 18:38:32 -0300

    corrigindo db

--- academicos/Banco de Dados 2/crud/database.py
+++ academicos/Banco de Dados 2/crud/database.py
@@ -1,9 +1,11 @@
 import sqlite3
+from pathlib import Path
 
-DB_NAME = "truco.db"
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR / "truco.db"
 
 def get_connection():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -11,8 +13,8 @@ def criar_tabela():
     conn = get_connection()
     conn.execute("""
         CREATE TABLE IF NOT EXISTS jogadores (
-            id   INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT    NOT NULL
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL
         )
     """)
     conn.commit()
